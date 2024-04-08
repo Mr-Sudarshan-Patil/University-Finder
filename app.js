411ab5d792c13034cf1fc11e31807da2b99da623
Sudarshan Patil <105053040+Mr-Sudarshan-Patil@users.noreply.github.com>
2024-04-08 22:29:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 let url = "http://universities.hipolabs.com/search?name=";
 
 let btn = document.querySelector("button");
-let input= document.querySelector("input");
+let input = document.querySelector("input");
 
+btn.addEventListener("click", async () => {
+    let country = input.value.trim(); 
 
-btn.addEventListener("click", async ()=>{
-    let country = input.value;
+    if (country === "") {
+       
+        alert("Please enter a country name.");
+        return;
+    }
 
-    let colArr = await getUnivercity(country)
+    let colArr = await getUniversity(country);
     console.log(colArr);
     Show(colArr);
-})
+});
 
-function Show(colArr){
+function Show(colArr) {
     let ul = document.querySelector("ul");
-    ul.innerText=""
-    for(col of colArr){
+    ul.innerText = "";
+    for (col of colArr) {
         console.log(col.name);
         let li = document.createElement("li");
         li.innerText = col.name;
@@ -23,14 +28,12 @@ function Show(colArr){
     }
 }
 
-async function getUnivercity(country){
-   try{
-    let res = await axios.get(url+country);
-    return res.data;
-
-   } catch(e){
-    console.log("error: ", e);
-    return [];
-   }
-  
+async function getUniversity(country) {
+    try {
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch (e) {
+        console.log("error: ", e);
+        return [];
+    }
 }
